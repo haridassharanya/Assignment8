7f1d54c856b1c8383122269e9e1e7d13eef2e9f3
Sharanya Haridas <sharanya0211@gmail.com>
2014-04-24 23:54:33 -0700

    basic javascript code

---
+++ scripts.js
@@ -0,0 +1,89 @@
+/**
+ * @author Sharanya Haridas
+ */
+
+
+var stemURL = "https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1Kg4n_bZlh1j5xK_KG8KxJWFjRz4Ia4r-HS8-iCUZ+WHERE+DATE%3E%20";
+var thekey = "&key=AIzaSyDo19_AYa5DRvvwPl9RmRIlsSxnmlbklqg";
+
+
+
+function displayNewData(e) { 
+	
+	var mID = e.target.id; 
+	console.log(mID);
+	
+
+	
+	var mNameArray = mID.split("_");
+	console.log(mNameArray);
+	
+	var mYear = mNameArray[1]; 
+	
+	
+	$.get(stemURL+"'"+mYear+"-12-01'"+thekey, dataLoaded, "json");
+	
+	
+}
+
+
+function dataLoaded(UNEMP) {
+	console.log(UNEMP);
+	
+	
+
+	var gTable = new google.visualization.DataTable();
+
+
+
+	gTable.addColumn('string', UNEMP.columns[0]);
+	gTable.addColumn('number', UNEMP.columns[1]);
+
+
+	gTable.addRows(UNEMP.rows);
+
+
+
+	var mOptions = {
+		title : "Unemployment Data"
+	};
+
+
+	
+	var gChart = new google.visualization.LineChart(document.getElementById("mChartDiv"));
+	
+
+
+	gChart.draw(gTable, mOptions);
+
+}
+
+
+
+function gVizloaded() {
+
+	console.log("google visualization is loaded!");
+	
+
+	$(".btn-success").on("click", displayNewData);
+
+
+	
+$("#year_2000").click();
+
+
+}
+
+
+function gDone() {
+	console.log("page done!");
+
+
+	google.load("visualization", "1", {
+		packages : ["corechart"],
+		"callback" : gVizloaded
+	});
+}
+
+
+$(document).ready(gDone);
